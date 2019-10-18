d971b3f92db6ae287f03ec05becb576cc5990993
Alex <ayzen1@student.monash.edu>
2019-10-18 14:19:15 +1100

    Finish prismic tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const Index = ({ data: { prismicHomepage } }) => (
+  <React.Fragment>
+    <h1>{prismicHomepage.data.title.text}</h1>
+    <div
+      dangerouslySetInnerHTML={{ __html: prismicHomepage.data.content.html }}
+    />
+  </React.Fragment>
+)
+
+export default Index
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    prismicHomepage {
+      data {
+        title {
+          text
+        }
+        content {
+          html
+        }
+      }
+    }
+  }
+`
